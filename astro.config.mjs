e9db95a945e4c4a900e0bfde3da40bddfa210409
Mariano <99189872+MarianoFfonseca@users.noreply.github.com>
2023-12-03 23:37:23 -0500

    fixing size of success page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
-
 import preact from "@astrojs/preact";
-
+import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "server",
-  adapter: netlify()
+  // adapter: netlify()
+  adapter: node({
+    mode: "standalone",
+  }),
 });
