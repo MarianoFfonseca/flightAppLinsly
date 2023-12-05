8e7e5092ea765f6231cb8e2505d4c5ac0df9a1c9
Mariano <99189872+MarianoFfonseca@users.noreply.github.com>
2023-12-04 20:34:05 -0500

    master branch changed to main

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 import preact from "@astrojs/preact";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "server",
-  // adapter: netlify()
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
 });
