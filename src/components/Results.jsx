7a73c03d4a3057a9567da66639933e820f6b4e06
Mariano <99189872+MarianoFfonseca@users.noreply.github.com>
2023-12-10 22:51:02 -0500

    Fix package.json scripts and add delete functionality

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,9 @@
 import { useState } from "preact/hooks";
 import { sortByTime } from "../lib/sortByTime";
+import { client } from "../lib/tursoDb";
 function Results({ allFlights }) {
   const [filtredFlights, setFiltredFlights] = useState(allFlights);
+  const [sureToDelete, setSureToDelete] = useState(false);
 
   const handleChangeSerchByName = (e) => {
     const value = e.target.value;
@@ -37,11 +39,21 @@ function Results({ allFlights }) {
     const sortedByReturning = sortByTime(filtredFlights.rows, "Return", value);
     setFiltredFlights({ rows: sortedByReturning });
   };
+  function deleteStudentPlans(plansId) {
+    if (sureToDelete === true) {
+      client.execute({
+        sql: "DELETE FROM flightInformation WHERE id = :id",
+        args: { id: plansId },
+      });
+      window.location.reload();
+    } else {
+      setSureToDelete(true);
+    }
+  }
 
-  console.log(filtredFlights.rows);
   return (
     <>
-      <div class="pb-4 bg-white dark:bg-gray-900">
+      <div class="pb-4 ">
         <label for="table-search" class="sr-only">
           Search
         </label>
@@ -72,15 +84,15 @@ function Results({ allFlights }) {
           />
         </div>
       </div>
-      <ul class="grid max-w-3xl mb-6 items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-          <div class="flex items-center ps-3">
+      <ul class="grid bg-gray-50 max-w-3xl mb-6 items-center w-full text-sm font-medium text-gray-900  border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        <li class=" pl-3 w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+          <div class="flex items-center ">
             <input
               id="vue-checkbox-list"
               type="checkbox"
               onClick={handleClickNeedsTransportation}
               value=""
-              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              class="w-4 h-4 text-blue-600 bg-gray-50 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             />
             <label
               for="vue-checkbox-list"
@@ -91,24 +103,24 @@ function Results({ allFlights }) {
           </div>
         </li>
         <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-          <div class="flex items-center ps-3">
+          <div class="flex items-center ">
             <select
               id="leaving"
               onChange={handleChangeFilterByLeaving}
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option selected>Filter by leaving</option>
+              <option selected>Filter by departure</option>
               <option value="leatest">Leaving leatest</option>
               <option value="earliest">Leaving earliest</option>
             </select>
           </div>
         </li>
         <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-          <div class="flex items-center ps-3">
+          <div class="flex items-center ">
             <select
               id="returning"
               onChange={handleChangeFilterByReturn}
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               <option selected>Filter by return</option>
               <option value="leatest">Returning latter</option>
@@ -141,6 +153,9 @@ function Results({ allFlights }) {
             <th scope="col" class="px-6 py-3">
               Student phone
             </th>
+            <th scope="col" class="px-6 py-3">
+              Done?
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -168,6 +183,12 @@ function Results({ allFlights }) {
                 <td class="px-6 py-4">{flight.email}</td>
 
                 <td class="px-6 py-4">{flight.phone}</td>
+                <td
+                  onClick={() => deleteStudentPlans(flight.id)}
+                  class="px-6 py-4 text-red-800 underline cursor-pointer"
+                >
+                  {sureToDelete ? "Sure?" : "Delete"}
+                </td>
               </tr>
             ))
           ) : (
